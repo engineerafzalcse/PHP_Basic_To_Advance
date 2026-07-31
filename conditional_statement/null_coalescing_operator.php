af031b7d8fc0dabc3e1bd7012e54625df0d5d302
engineerafzalcse <mdafzalhossen16813@gmail.com>
2026-08-01 00:08:30 +0600

    null coalescing statement

---
+++ conditional_statement/null_coalescing_operator.php
@@ -0,0 +1,37 @@
+<?php
+
+
+echo "<h3>PHP Conditional Statements (Full Guide)</h3>";
+
+echo "<h4>Types of Conditional Statements : </h4>";
+echo "1. if</br>";
+echo "2. if...else</br>";
+echo "3. if...elseif...else</br>";
+echo "4. Nested if</br>";
+echo "5. Short Hand if</br>";
+echo "6. Ternary Operator</br>";
+echo "7. Null Coalescing Operator</br></br>";
+
+echo "7. Null Coalescing Operator (??)</br>";
+
+
+$username = $_GET['username'] ?? "Guest";
+
+echo "</br>".$username."</br>";   // যদি $_GET['username'] না থাকে, তাহলে "Guest" দেখাবে।
+
+
+
+// Empty() Condition
+$name = "";
+if(empty($name)){
+    echo "Empty</br>";
+}//end
+
+
+echo "isset()";
+if(isset($_POST['email'])){
+    echo "Found</br>";
+}
+
+echo "</br>End ------------ End</br></br>";
+?>
