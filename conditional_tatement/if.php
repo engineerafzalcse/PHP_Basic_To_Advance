ad3c8f82ea0c524c2c70567d239fe9d1795959c6
engineerafzalcse <mdafzalhossen16813@gmail.com>
2026-07-31 20:21:00 +0600

    if condition example

---
+++ conditional_tatement/if.php
@@ -0,0 +1,46 @@
+<?php
+
+// php print type system
+echo "<h3>PHP Conditional Statements (Full Guide)</h3>";
+
+echo "<h4>Types of Conditional Statements : </h4>";
+echo "1. if</br>";
+echo "2. if...else</br>";
+echo "3. if...elseif...else</br>";
+echo "4. Nested if</br>";
+echo "5. Short Hand if</br>";
+echo "6. Ternary Operator</br>";
+echo "7. Null Coalescing Operator</br></br>";
+
+echo "1. if Statement</br>";
+
+// if (condition) {
+    //     // code
+    // }
+    
+$age = 20;
+if ($age >= 18) {
+    echo "You are Adult</br>";
+}
+
+
+
+$age = 15;
+if ($age >= 18) {
+    echo "Adult</br>";
+}
+echo "Finished</br>";
+
+
+$number = 100;
+
+if ($number > 50){
+    echo "Big Number</br>";
+}
+
+echo "</br>End ------------ End</br></br>";
+
+
+
+
+?>
