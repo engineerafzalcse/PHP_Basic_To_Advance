2ca6b0e0d6d1687c26afcda0b71d9f3e689f250e
engineerafzalcse <mdafzalhossen16813@gmail.com>
2026-07-31 20:52:47 +0600

    short hand if statement

---
+++ conditional_tatement/short_hand_if.php
@@ -0,0 +1,24 @@
+<?php
+
+
+echo "<h3>PHP Conditional Statements (Full Guide)</h3>";
+
+echo "<h4>Types of Conditional Statements : </h4>";
+echo "1. if</br>";
+echo "2. if...else</br>";
+echo "3. if...elseif...else</br>";
+echo "4. Nested if</br>";
+echo "5. Short Hand if</br>";
+echo "6. Ternary Operator</br>";
+echo "7. Null Coalescing Operator</br></br>";
+
+echo "5. Short Hand if</br>";
+
+$age = 20;
+
+if($age >=18) echo "Adult</br>";
+
+
+
+echo "</br>End ------------ End</br></br>";
+?>
