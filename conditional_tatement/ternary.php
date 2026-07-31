742f6b23c1ebf7bcd7ee7540beb7f7ab6d2d5c8e
engineerafzalcse <mdafzalhossen16813@gmail.com>
2026-07-31 20:58:46 +0600

    ternary operator

---
+++ conditional_tatement/ternary.php
@@ -0,0 +1,28 @@
+<?php
+
+
+echo "<h3>PHP Conditional Statements (Full Guide)</h3>";
+
+echo "<h4>Types of Conditional Statements : </h4>";
+echo "1. if</br>";
+echo "2. if...else</br>";
+echo "3. if...elseif...else</br>";
+echo "4. Nested if</br>";
+echo "5. Short Hand if</br>";
+echo "6. Ternary Operator</br>";
+echo "7. Null Coalescing Operator</br></br>";
+
+echo "6. Ternary Operator(Short if else)</br>";
+
+
+// condition ? true : false;
+$age = 17;
+
+echo ($age>=18) ? "Adult</br>" : "Child</br>";
+
+$status = true;
+
+echo $status ? "Active" : "Inactive";
+
+echo "</br>End ------------ End</br></br>";
+?>
