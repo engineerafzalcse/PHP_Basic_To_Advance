dfafacbb5df3df8b0b21125a435fd75bec88b25b
engineerafzalcse <mdafzalhossen16813@gmail.com>
2026-07-31 20:24:08 +0600

    if else statement

---
+++ conditional_tatement/if_else.php
@@ -0,0 +1,47 @@
+<?php
+
+// php print type system
+echo "<h3>PHP Conditional Statements (Full Guide)</h3>";
+
+echo "<h4>Types of Conditional Statements : </h4>";
+echo "1. if</br>";
+echo "2. if...else</br>";
+echo "3. if...elseif...else</br>";
+echo "4. Nested if</br>";
+echo "5. Short Hand if</br>";
+echo "6. Ternary Operator</br>";
+echo "7. Null Coalescing Operator</br></br>";
+
+
+echo "2. if...else Statement</br>";
+$age = 15;
+
+if($age >=18){
+
+    echo "Adult</br>";
+
+}else{
+
+    echo "Child</br>";
+
+}
+
+
+
+$marks = 80;
+
+if($marks >= 33){
+
+    echo "Pass</br>";
+
+}else{
+
+    echo "Fail</br>";
+
+}
+echo "</br>End ------------ End</br></br>";
+
+
+
+
+?>
