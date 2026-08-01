dc2da1d3dfb2754dd736bb185890c025009ec331
engineerafzalcse <mdafzalhossen16813@gmail.com>
2026-08-01 18:49:43 +0600

    do while loop

---
+++ loop/do_while.php
@@ -0,0 +1,37 @@
+<?php
+echo "<h3>PHP Loop (Complete Guide)</h3>";
+
+echo "<h4>Loop হলো এমন একটি Control Structure যা একই কাজ বারবার (Repeatedly) করতে ব্যবহার করা হয়, যতক্ষণ না নির্দিষ্ট Condition False হয়।</h4>";
+echo "<h4>PHP-তে কত ধরনের Loop আছে?</h4>";
+
+echo "<h4>PHP-তে 4 ধরনের Loop আছে।</h4>";
+echo "<h5>1. for Loop</h5>";
+echo "<h5>2. while Loop</h5>";
+echo "<h5>3. do...while Loop</h5>";
+echo "<h5>4. foreach Loop</h5>";
+echo "<h5>এছাড়াও : break, continue, nested loop এগুলিও Loop-এর গুরুত্বপূর্ণ অংশ।</h5>";
+echo "</br>";
+echo "<h4>Loop কিভাবে কাজ করে? : প্রতিটি Loop মূলত ৩টি বিষয়ের উপর কাজ করে।</h4>";
+echo "<h5>১. Initialization</h5>";
+echo "<h5>২. Condition</h5>";
+echo "<h5>৩. Increment / Decrement</h5>";
+
+
+echo "<h4>3. do...while Loop : টি কমপক্ষে একবার Code Execute করে।</h4>";
+echo "<h5>1 থেকে 5</h5>";
+
+$i = 1;
+do{
+    echo $i."<br>";
+    $i++;
+}while($i <= 5);
+
+echo "</br>";
+
+$i = 10;
+do{
+    echo $i."<br>";
+    $i--;
+}while($i >= 1);
+
+?>
